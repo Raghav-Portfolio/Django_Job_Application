d4e2040003d55047430f6d73e58ab74f03ce442c
Raghav <51016731+Raghav-Portfolio@users.noreply.github.com>
2024-10-18 18:14:12 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ragkan1499@gmail.com'
-EMAIL_HOST_PASSWORD = "sadadwdawdg"
+EMAIL_HOST_USER = '' # Enter your details here
+EMAIL_HOST_PASSWORD = ""
